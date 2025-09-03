2295991c27fca82fad7045cfc9504fbe9e172c8f
ikeermtzz <ikermtzfimbres@gmail.com>
2025-09-02 18:04:12 -0600

    Update Programa Juego 'Adivina el Numero'.py

--- Programa Juego 'Adivina el Numero'.py
+++ Programa Juego 'Adivina el Numero'.py
@@ -1,5 +1,6 @@
 #Juego Adivina el numero
 from random import *
+numero_a_adiv = 0 #Para que no haya algun error por no estar definido
 numero = randint(1,101)
 nombre = input("Cual es tu nombre: ")
 print(f"{nombre} he pensado un numero del 1 al 100 y tienes 8 intentos para adivinarlo ")
@@ -20,4 +21,4 @@ while intentos <= 7: #8 intentos
             print(f"Incorrecto, el numero es mayor")
 # Si se gastan los 8 intentos sin adivinar
 if numero_a_adiv != numero:
-    print(f"😢 Te quedaste sin intentos. El número era {numero}.")
+    print(f" Te quedaste sin intentos. El número era {numero}.")
